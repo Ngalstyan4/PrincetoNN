2038860e50d3a731b3b12d074f4b779e4c3591dd
Narek Galstyan <ngalstyan4@gmail.com>
2017-04-02 07:56:36 -0400

    semantic fixes

--- src/piazza.js
+++ src/piazza.js
@@ -1,11 +1,13 @@
 var pz = require('piazza-api');
+var password = require("./password.piazza");
+
 // Handle errors
 function error(err) {
 	console.log("Error in code ",err);
 }
 
 
-var pzme = pz.login('narekg@princeton.edu', '5296e988eb09863e4ec57c77f252778a');
+var pzme = pz.login('narekg@princeton.edu', password);
 
 var piazza = {};
 piazza.piazza_whatCourses = (semester, callback) => {
